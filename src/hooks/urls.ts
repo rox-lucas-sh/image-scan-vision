50d9d13ba676be5d757c084880cdaf8a17037d17
Lucas Schwalm <lucas.schwalm@roxcode.dev>
2025-08-15 14:01:08 -0300

    chore: hive on production

--- src/hooks/urls.ts
+++ src/hooks/urls.ts
@@ -1,2 +1,7 @@
-export const ScanHost = "http://localhost:2020";
+// // Local
+// export const ScanHost = "http://localhost:2020";
+// export const MotorHost = "https://test.motor.roxwallet.roxcode.io";
+
+// Prod
+export const ScanHost = "http://api.hive.roxcode.io";
 export const MotorHost = "https://test.motor.roxwallet.roxcode.io";
